523674915d3226a7d9c6b7748149b0f0cfeb4b5c
Sérgio Lima <sergiosdlima@gmail.com>
2016-10-12 22:34:45 -0300

    Implement Store, Product, Price and User schema.

--- index.js
+++ index.js
@@ -1,23 +1,77 @@
 var express      = require('express');
+var bodyParser   = require('body-parser');
 var app          = express();
 var mongoose     = require('mongoose');
 var mongoosastic = require('mongoosastic');
 var Schema       = mongoose.Schema;
 
-mongoose.connect(process.env.MONGO_URL);
+// Configuration
+app.use(bodyParser());
+
+// Model/Schema
+mongoose.connect(process.env.MONGO_URL, function(err) {
+  if (err) {
+    console.error(err);
+  }
+  console.log('connected.... unless you see an error the line before this!');
+});
+
+var StoreSchema = new Schema({
+  name  : String,
+  local : String
+});
+
+var ProductSchema = new Schema({
+  description   : String,
+  brand         : String,
+  category      : String,
+  type          : String,
+  picture       : String
+});
+
+var PriceSchema = new Schema({
+  products: {
+    type        : Schema.Types.ObjectId,
+    ref         : 'Product',
+    es_schema   : ProductSchema,
+    es_indexed  : true,
+    es_select   : 'description brand category type'
+  },
+  stores: {
+    type        : Schema.Types.ObjectId,
+    ref         : 'Store',
+    es_schema   : StoreSchema,
+    es_indexed  : true,
+    es_select   : 'name local'
+  },
+  price         : Number,
+  created_at    : {type : Date, default : Date.now}
+});
 
 var UserSchema = new Schema({
-    name: {type:String, es_indexed:true}
-  , email: String
-  , city: String
-})
+  name          : {type : String, es_indexed : true},
+  email         : String,
+  city          : {type : String, es_indexed : true}
+});
 
 UserSchema.plugin(mongoosastic, {
   host: process.env.ELASTICSEARCH_URL,
   port: 9200
 });
 
+PriceSchema.plugin(mongoosastic, {
+  host: process.env.ELASTICSEARCH_URL,
+  port: 9200,
+  populate: [
+    {path: 'products', select: 'description brand category type'},
+    {path: 'stores', select: 'name local'}
+  ]
+});
+
 var UserModel = mongoose.model("User", UserSchema);
+var ProductModel = mongoose.model("Product", ProductSchema);
+var StoreModel = mongoose.model("Store", StoreSchema);
+var PriceModel = mongoose.model("Price", PriceSchema);
 
 app.get('/', function (req, res) {
   res.send('Hello World!');
